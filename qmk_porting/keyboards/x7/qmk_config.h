50cb21a34367e0ed4088c5f2ede00f81176c0e9d
rcx123 <65963294+rcx123@users.noreply.github.com>
2023-07-16 20:38:01 +0800

    Add files via upload

--- qmk_porting/keyboards/x7/qmk_config.h
+++ qmk_porting/keyboards/x7/qmk_config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0B92
 #define DEVICE_VER   0x0001
 #define MANUFACTURER CX_Studio
-#define PRODUCT      Xtreme7
+#define PRODUCT      Xtreme_7
 
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 4
